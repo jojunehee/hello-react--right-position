688c646e4d79a5d8bee6fa506e84707b53304d2d
jojunhee <chojun1108@handong.ac.kr>
2024-11-05 11:32:57 +0900

    i made a react project

---
+++ src/component1.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class component1 extends Component {
+  render() {
+    return (
+      <div>component1</div>
+    )
+  }
+}
---
+++ src/component2.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class component2 extends Component {
+  render() {
+    return (
+      <div>component2</div>
+    )
+  }
+}
+
+export default component2
